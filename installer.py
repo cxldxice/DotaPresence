4d1c718e3031ad0b0019678cf91ce8dc5d4072f0
cxldxice <cxldxfxtxre@gmail.com>
2021-10-05 10:53:04 +0300

    installer added

---
+++ installer.py
@@ -0,0 +1,36 @@
+config_data = \
+"""DotaPresence  Configuration"
+{
+    "uri"               "http://localhost:6768/"
+    "timeout"           "15.0"
+    "buffer"            "15.0"
+    "throttle"          "15.0"
+    "heartbeat"         "15.0"
+    "data"
+    {
+        "provider"      "1"
+        "map"           "1"
+        "player"        "1"
+        "hero"          "1"
+    }
+}"""
+
+
+def main():
+    print("[+] Dota path")
+    path = str(input("[>] ")) #deafoult > C:/Program Files (x86)/Steam/steamapps/common/dota 2 beta
+
+    try:
+        with open(
+            f"{path}/game/dota/cfg/gamestate_integration/gamestate_integration_rpc.cfg", "w"
+        ) as config:
+            config.write(config_data)
+
+        print("[+] Success")
+
+    except FileNotFoundError:
+        print("[-] Path not found")
+
+
+if __name__ == "__main__":
+    main()
--- server.py
+++ server.py
@@ -3,23 +3,21 @@ import json
 from flask import Flask, \
     redirect, render_template, request
 
-
-
 app = Flask(__name__)
 
 
 @app.route("/", methods=["POST", "GET"])
 def index():
     if request.method == "POST":
-        json_data = json.loads(request.data)
+        print("[+] Data recived")
 
+        json_data = json.loads(request.data)
         with open("info.json", "w") as info_file:
             json.dump(json_data, info_file)
         
-
     return "0"
 
 
 if __name__ == "__main__":
-    print("[+] Server started!")
+    print("[+] Server started")
     app.run(port=6768)
