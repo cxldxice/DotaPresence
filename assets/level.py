e360ea9e63dad0e598183940153b50ce1de31239
Evgeniy <Xorsiphus@yandex.ru>
2021-10-06 12:58:09 +0700

    level icons generation update

--- assets/level.py
+++ assets/level.py
@@ -4,19 +4,22 @@ from PIL import Image, ImageDraw, ImageFont
 def write_center(text, font, draw, area, fill):
     (w, h) = font.getsize(text)
 
-    draw.text(((area[0]-w)/2,(area[1]-h-40)/2), text, font=font, fill=fill)
+    draw.text(((area[0]-w)/2, (area[1]-h-40)/2), text, font=font, fill=fill)
 
 
 def draw(level):
     image = Image.new("RGBA", (512, 512), color=(0, 0, 0, 0))
     draw = ImageDraw.Draw(image)
 
-    font = ImageFont.truetype("./assets/Montserrat-SemiBold.ttf", 230)
+    font = ImageFont.truetype("./Trajan Bold.ttf", 230)
 
-    draw.rounded_rectangle((0, 0, 512, 512), 256, "#242424")
-    write_center(str(level), font, draw, (512, 512), "#ffffff")
+    # draw.rounded_rectangle((0, 0, 512, 512), 256, "#d0bda5")
+    draw.rounded_rectangle((0, 0, 512, 512), radius=256,
+                           fill="#242424", outline="#d0bda5", width=25)
+    write_center(str(level), font, draw, (512, 512), "#d9af7a")
 
-    image.save(f"./assets/levels/{level}.png", "PNG")
+    image.save(f"./levels/{level}.png", "PNG")
 
-for level in range(1, 30+1):
+
+for level in range(1, 30 + 1):
     draw(level)
